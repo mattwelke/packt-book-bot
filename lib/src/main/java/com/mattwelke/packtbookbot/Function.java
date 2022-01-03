b8f3b92aa82f452b6a33985299d0f2e752956b50
Matt Welke <mattwelke@gmail.com>
2022-01-03 18:02:49 -0500

    Change Tweet format to include publication date and author(s), use Google to get product page URL instead of Packt search results.

--- lib/src/main/java/com/mattwelke/packtbookbot/Function.java
+++ lib/src/main/java/com/mattwelke/packtbookbot/Function.java
@@ -19,6 +19,7 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 /**
@@ -29,7 +30,6 @@ public class Function extends Action {
     private static final String freeLearningURL = "https://www.packtpub.com/free-learning";
     private static final String twitterURL = "https://api.twitter.com/2/tweets";
     private static final String titleSelector = "h3.product-info__title";
-    private static final String productLinkSelector = "a.product-item-link";
 
     // Secrets - rotate before making repo public
     private static final String consumerKey = "YPn8qTcTKp983e5cjoiJ733xa";
@@ -64,27 +64,35 @@ public class Function extends Action {
 
             var freeBookTitle = freeBookEls.first().text().replace("Free eBook - ", "");
 
+            var author = freeBookDoc.select(".free_learning__author").first().html().trim().replace("By ", "");
+
             System.out.println("Done parsing. Free book of the day is " + freeBookTitle + ".");
 
-            // Use title to get link to product page
-            String searchURL = "https://www.packtpub.com/catalogsearch/result/?q=%22"
-                    + URLEncoder.encode(freeBookTitle, StandardCharsets.UTF_8) + "%22";
+            var freeBookURL = productPageURL(freeBookTitle);
 
-            Document searchDoc = Jsoup.connect(searchURL).get();
-            Elements searchEls = searchDoc.select(productLinkSelector);
+            var productPageDoc = Jsoup.connect(freeBookURL).get();
 
-            String freeBookURL = searchEls.first().attr("href");
+            // Author(s)
+            var authorsEl = productPageDoc.select(".product-info__author").first();
+            var authors = authorsEl.html().trim().replace("By ", "").replace(" , ", ", ");
+            var authorsStrBase = authors.contains(",") ? "Authors" : "Author";
+            var authorsStr = String.format("%s: %s", authorsStrBase, authors);
+
+            // Publication date
+            var datalistEl = productPageDoc.select(".overview__datalist").first();
+            String pubDate = datalistEl.children().stream()
+                .filter((Element el) -> el.select(".datalist__name").html().toLowerCase().contains("publication date"))
+                .findFirst().get().select(".datalist__value").html();
 
             String tweet = String.format(
-                    "%s is the free eBook of the day from Packt!\\n\\Visit %s to claim the eBook and %s for more info about the title.",
-                    freeBookTitle, freeLearningURL, freeBookURL);
+                    "%s (%s, %s) is the free eBook of the day from Packt!\\n\\nVisit %s to claim the eBook and %s for more info about the title.",
+                    freeBookTitle, pubDate, authorsStr, freeLearningURL, freeBookURL);
             System.out.println("Finished tweet = " + tweet);
 
             postToTwitter(tweet);
 
-            System.out.println("Done posting to twitter.");
-        }
-         catch (Exception e) {
+            System.out.println("Finished posting to twitter.");
+        } catch (Exception e) {
             e.printStackTrace();
             System.err.println("Error: " + e.getMessage());
         }
@@ -106,7 +114,26 @@ public class Function extends Action {
         request.setEntity(new StringEntity("{\"text\":\"" + tweetBody + "\"}"));
 
         HttpResponse response = httpClient.execute(request);
-        System.out.println("Twitter API request complete. Response status = " +
-                response.getStatusLine());
+        System.out.println("Finished posting to Twitter. Response code from API request: " +
+                response.getStatusLine().getStatusCode());
+    }
+
+    private static String productPageURL(String title) throws IOException {
+        Document searchDoc = Jsoup.connect(googleSearchURL(title)).get();
+        Elements searchEls = searchDoc.select("h3");
+
+        // Find the h3 that represents the search result for the title
+        Element titleSearchResult = searchEls.stream()
+                .filter((Element res) -> res.html().contains(String.format("%s | Packt", title)))
+                .findFirst().get();
+
+        // Get the link from the search result - parent element is <a>
+        return titleSearchResult.parent().attr("href");
+    }
+
+    private static String googleSearchURL(String title) {
+        var googleSearchQuery = String.format("\"%s\" site:packtpub.com -site:subscription.packtpub.com", title);
+        return String.format("https://google.com/search?q=%s",
+                URLEncoder.encode(googleSearchQuery, StandardCharsets.UTF_8));
     }
 }
