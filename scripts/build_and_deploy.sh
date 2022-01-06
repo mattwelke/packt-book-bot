89e7e5e005fc83c23e74382d8418eb212936e68c
Matt Welke <mattwelke@gmail.com>
2022-01-05 23:40:48 -0500

    Rotate secrets. Refactor to get secrets from action params. Add GitHub Actions deploy workflow.

--- scripts/build_and_deploy.sh
+++ scripts/build_and_deploy.sh
@@ -1,5 +1,11 @@
 #/bin/bash
 
+# Deploys the function to IBM Cloud Functions using the Java 17 runtime
+# from the extended runtimes project.
+#
+# Script uses command line params instead of env vars, to help with
+# running it locally too, not just with CI/CD.
+
 REGION="us-south"
 RESOURCE_GROUP="book-bot"
 FUNCTIONS_NAMESPACE="book-bot"
@@ -12,6 +18,8 @@ rm -r $BUILD_DIR 2> /dev/null
 
 ./gradlew shadowJar
 
+ibmcloud login --apikey $1
+
 ibmcloud target -r $REGION
 ibmcloud target -g $RESOURCE_GROUP
 ibmcloud fn namespace target $FUNCTIONS_NAMESPACE
@@ -19,4 +27,8 @@ ibmcloud fn namespace target $FUNCTIONS_NAMESPACE
 # Do deploy using action update (aka create or update) command
 ibmcloud fn action update $ACTION_NAME $JAR_PATH \
   --main "com.mattwelke.packtbookbot.Function" \
-  --docker "owextendedruntimes/java-17:experiment-abstract-class-impl-1634521420"
+  --docker "owextendedruntimes/java-17:experiment-abstract-class-impl-1634521420" \
+  --param twitterConsumerKey $2 \
+  --param twitterConsumerSecret $3 \
+  --param twitterToken $4 \
+  --param twitterTokenSecret $5
