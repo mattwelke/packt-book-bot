c5bccd7f38fae8ac87e2bb4b0240036a9d8b73c8
Matt Welke <mattwelke@gmail.com>
2022-04-23 03:12:06 -0400

    Update action code to Java 17 and update runtime used to Java 18.

--- title-fetcher/src/main/java/com/mattwelke/packtbookbot/FreeLearningPageDataFetcher.java
+++ title-fetcher/src/main/java/com/mattwelke/packtbookbot/FreeLearningPageDataFetcher.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.mattwelke.packtbookbot.Urls.FREE_LEARNING;
 
